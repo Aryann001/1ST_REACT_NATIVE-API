d92d8628a3831e690d05fb02c9b37f864b62e56c
Aryann001 <140709829+Aryann001@users.noreply.github.com>
2023-11-09 18:24:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.use(cookieParser());
 app.use(fileUpload());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URI],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
